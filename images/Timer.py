736fba98bf2e088ae6702c5dadf3ab68316dc43b
Joseph <jas759@txstate.edu>
2024-09-26 11:56:39 -0500

    Started work on basic in-game timer

---
+++ images/Timer.py
@@ -0,0 +1,19 @@
+# Timer represents the in-game clock that runs anytime
+# the game has been started and has not been paused.
+class Timer:
+    def __init__(self):
+        self.elapsed_time = 0.0
+        self.stopped = True
+
+    def start(self):
+        self.stopped = False
+
+    def stop(self):
+        self.stopped = True
+
+    def reset(self):
+        self.elapsed_time = 0.0
+        self.stopped = True
+
+    def update(self, delta_time):
+        self.elapsed_time = self.elapsed_time + delta_time
