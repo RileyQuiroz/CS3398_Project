e7bd9062bfabf43815b85e7bdd74d7476ee8ba3b
Riley Quiroz <fjq11@txstate.edu>
2024-09-26 00:55:33 -0500

    Skeleton of save/load functionality added

--- savesystem/user_save_and_load.py
+++ savesystem/user_save_and_load.py
@@ -5,6 +5,8 @@ from save_progress import *
 from load_progress import *
 # will import all variables to be saved when they exist from the file they exist under
 
+
+
 save_state = {
     # Temporary variable names, will change and add more according to other files
     # Saved values will also be changed to reflect in-game values once those parts of code are finished
@@ -15,4 +17,13 @@ save_state = {
     "score": 0
 }
 
-# Users will be given the oportunity to save after every level completes
+# Users will be given the oportunity to save after every level completes or when they quit
+# File gets called by main game, then runs either save or load
+# structured as is so that testing is possible on my branch
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_s:
+            save_game()
+        if event.key == pygame.K_l:
+            load_game()
+
