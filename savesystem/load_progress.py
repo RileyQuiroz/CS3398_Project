7f8c95433a71cb7544dcd7d97611c8ef70eaf75f
Riley Quiroz <fjq11@txstate.edu>
2024-09-29 17:19:33 -0500

    Updated imports for load_progress

--- savesystem/load_progress.py
+++ savesystem/load_progress.py
@@ -1,5 +1,7 @@
-import pygame
 import json
+import os
+
+from menu.main_menu import * # Used for draw_text formatting
 
 def load_game():
     # Attempt to load a file of the same name as the one in save_progress
