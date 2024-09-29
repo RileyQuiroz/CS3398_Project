49b720f1802ba2172a2e7fa320e828616324d7f5
Riley Quiroz <fjq11@txstate.edu>
2024-09-29 18:26:46 -0500

    load_game functionality added

--- savesystem/load_progress.py
+++ savesystem/load_progress.py
@@ -4,5 +4,14 @@ import os
 from menu.main_menu import * # Used for draw_text formatting
 
 def load_game(filename):
-    # Attempt to load a file of the same name as the one in save_progress
-    print("Unable to load file")
+    # Attempt to load the desired save file
+    file_path = os.path.join('savedata', filename)
+    if os.path.getsize(file_path) > 0:
+        with open(file_path, 'r') as load_file:
+            game_state = json.load(load_file)
+        draw_text('Save loaded', font, BLACK, screen, WIDTH // 2, HEIGHT // 2 - 100)
+        print("Load success")
+        return game_state
+    draw_text('No save data found', font, BLACK, screen, WIDTH // 2, HEIGHT // 2 - 100)
+    print("No savedata")
+    return 
