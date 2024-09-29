3d8dc6383790a56e840d2387c2724b637a81d507
Riley Quiroz <fjq11@txstate.edu>
2024-09-29 15:43:46 -0500

    Successful save will now be displayed alongside terminal message

--- savesystem/save_progress.py
+++ savesystem/save_progress.py
@@ -1,6 +1,9 @@
 import pygame
 import json
 
+from menu.main_menu import *
+
 def save_game():
     # Will write all variables in save_state to a json file to be accessed
+    draw_text('Game Saved', font, BLACK, screen, WIDTH // 2, HEIGHT // 2 - 100)
     print("Saving is WIP")
