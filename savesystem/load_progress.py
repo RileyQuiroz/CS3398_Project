4bbe28f715832eef24baf1b970977f7519c5103f
Riley Quiroz <fjq11@txstate.edu>
2024-10-02 18:32:12 -0500

    Load function now has output in window

--- savesystem/load_progress.py
+++ savesystem/load_progress.py
@@ -18,7 +18,7 @@ def draw_text(text, font, color, surface, x, y):
 
 def load_game(filename):
     # Attempt to load the desired save file
-    file_path = os.path.join('savedata', filename)
+    file_path = os.path.join('savesystem/savedata', filename)
     if os.path.getsize(file_path) > 0:
         with open(file_path, 'r') as load_file:
             game_state = json.load(load_file)
