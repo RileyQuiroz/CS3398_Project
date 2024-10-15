00b4378f24cca3b67e1208710928f244354697e4
Riley Quiroz <fjq11@txstate.edu>
2024-10-15 17:33:48 -0500

    Basics for enemy structure added to class

--- enemies/enemy_structure.py
+++ enemies/enemy_structure.py
@@ -1,1 +1,14 @@
-# Will hold enemy class
+class Enemy:
+    def __init__(self):
+        self.health = 1
+        self.living = True
+        self.enemy_type = 0 # Will be used to determine the type of enemy
+        self.bullet_pattern = 0 # Determines bullet pattern for firing pellets
+        
+    def check_if_alive(self):
+        if(self.health < 1):
+            self.living = False
+            # Then remove the destroyed enemy
+            
+    def decrease_health(self):
+        self.health = self.health - 1
