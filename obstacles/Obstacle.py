90b1f8fdbf7073fc4ae5c38a71c5f3fc6438d577
Joseph <jas759@txstate.edu>
2024-10-15 18:04:09 -0500

    Updated collision detection in Obstacle class and added test file.

--- obstacles/Obstacle.py
+++ obstacles/Obstacle.py
@@ -1,9 +1,11 @@
+from math import sqrt
+
 # The Obstacle class represents an in-game object that can
 # impede the player in some way.
 class Obstacle:
     def __init__(self, radius, position, sprite):
         self.radius = radius
-        self.position = (0, 0)
+        self.position = position
         self.velocity = (0, 0)
         self.sprite = 0
         self.is_colliding = False
@@ -12,16 +14,19 @@ class Obstacle:
         pass
 
     def check_for_player_collision(self, player):
-        # Calculate the slope between the player's position and the
-        # obstacle's position. If the slope is less than the sum of
-        # the radii of the two objects, they are colliding.
-        slope = (player.position.y - self.position.y) / (player.position.x - self.position.x)
-        self.is_colliding = abs(slope) < self.player.radius
+        # Calculate the distance between the player's position and the
+        # obstacle's position. If the distance is less than the sum of
+        # the radii of the two objects, they are colliding
+        dist_x = self.position[0] - player.position[0]
+        dist_y = self.position[1] - player.position[1]
+        dist_root = sqrt((dist_x * dist_x) + (dist_y * dist_y))
+        self.is_colliding = dist_root <= (self.radius + player.radius)
 
     def update(self, player, dt):
         self.check_for_player_collision(player)
 
         if not self.is_colliding:
-            self.position.x = self.position.x + (self.velocity.x * dt)
-            self.position.y = self.position.y + (self.velocity.y * dt)
-
+            # If not colliding with the player, the obstacle is free to move
+            new_x_pos = self.position[0] + (self.velocity[0] * dt)
+            new_y_pos = self.position[1] + (self.velocity[1] * dt)
+            self.position = (new_x_pos, new_y_pos)
---
+++ obstacles/obstacle_test.py
@@ -0,0 +1,31 @@
+from Obstacle import *
+
+def display_position(obst):
+    print("Position: ( X:", obst.position[0], "Y:", obst.position[1], ")")
+
+def display_collision_status(obst):
+    if obst.is_colliding:
+        print("Player is colliding with the obstacle!")
+    else:
+        print("No collision detected!")
+
+# Initialize the in-game obstacle and player position
+test_obstacle = Obstacle(5, (2, 1), 0)
+player = Obstacle(7, (2, 2), 0)
+
+# Output the obstacle's current coordinates
+display_position(test_obstacle)
+
+# Output the obstacle's current collision status
+display_collision_status(test_obstacle)
+
+# Set movement velocity for the obstacle
+test_obstacle.velocity = (1, 1)
+
+# Update the obstacle using a dummy value for delta time
+test_obstacle.update(player, 0.01)
+
+# Output the obstacle's new coordinates and collision status
+display_position(test_obstacle)
+display_collision_status(test_obstacle)
+
