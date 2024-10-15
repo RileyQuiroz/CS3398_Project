dce85fa5a77e209577a4f56a982d1ae4dbac7d6a
Riley Quiroz <fjq11@txstate.edu>
2024-10-15 18:22:49 -0500

    More functionality added to class

--- enemies/enemy_structure.py
+++ enemies/enemy_structure.py
@@ -1,14 +1,36 @@
+import pygame
 class Enemy:
-    def __init__(self):
-        self.health = 1
+    def __init__(self, type = 0, health = 1, pattern = 0, position = (10, 10)):
+        self.type = type # Will not have type 0 for final product
+        self.health = health
         self.living = True
-        self.enemy_type = 0 # Will be used to determine the type of enemy
-        self.bullet_pattern = 0 # Determines bullet pattern for firing pellets
-        
-    def check_if_alive(self):
+        self.bullet_pattern = pattern # Determines projectile pattern
+        self.position = position
+            
+    def decrease_health(self, damage = 1): # returns true if enemy still alive, false otherwise
+        self.health -= damage
         if(self.health < 1):
             self.living = False
-            # Then remove the destroyed enemy
-            
-    def decrease_health(self):
-        self.health = self.health - 1
+        return self.living
+        
+    def fire_shot(self):
+        print("Can't shoot yet")
+        # if bullet pattern 0, ship type a pattern
+        # else if bullet pattern 1, ship type b pattern
+    
+    def move_enemy(self):
+        #if enemy type 1, use ship type a move pattern
+        # for now, just moves from right to left
+        mover = 1
+        if self.position >= 800:
+            mover = -1
+        else:
+            mover = 1
+        self.position += mover
+        print("on the move")
+        
+    def display_enemy(self, surface):
+        #if self.type = 0
+        pygame.draw.circle(surface) #color position, radius)
+
+    
