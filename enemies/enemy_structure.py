d41e5996476b7602ef1146fbc1d166cfae15d5be
Riley Quiroz <fjq11@txstate.edu>
2024-10-15 23:37:11 -0500

    Class modified to work with pygame.sprite module

--- enemies/enemy_structure.py
+++ enemies/enemy_structure.py
@@ -1,36 +1,36 @@
 import pygame
-class Enemy:
-    def __init__(self, type = 0, health = 1, pattern = 0, position = (10, 10)):
-        self.type = type # Will not have type 0 for final product
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, health = 1, pattern = 0, x = 10, y = 10):
+        super().__init__()
         self.health = health
         self.living = True
         self.bullet_pattern = pattern # Determines projectile pattern
-        self.position = position
+        self.color = (255, 0, 0) # Default red color, will change when we have sprites
+        self.pos_x = x
+        self.pos_y = y
+        self.size = 10
+        self.image = pygame.Surface((self.size, self.size)) #Next 3 lines are for ship's image on screen
+        self.image.fill(self.color)  
+        self.rect = self.image.get_rect(center=(self.x, self.y))
             
     def decrease_health(self, damage = 1): # returns true if enemy still alive, false otherwise
         self.health -= damage
         if(self.health < 1):
             self.living = False
-        return self.living
+            self.kill()
         
     def fire_shot(self):
         print("Can't shoot yet")
         # if bullet pattern 0, ship type a pattern
         # else if bullet pattern 1, ship type b pattern
     
-    def move_enemy(self):
-        #if enemy type 1, use ship type a move pattern
-        # for now, just moves from right to left
-        mover = 1
-        if self.position >= 800:
-            mover = -1
-        else:
-            mover = 1
-        self.position += mover
+    def update(self, screen_width): # Updates position, will be changed when working on first enemy type
+        if self.rect.right >= screen_width or self.rect.left <= 0:
+            self.velocity *= -1
+        self.rect.x += self.velocity  
         print("on the move")
         
-    def display_enemy(self, surface):
-        #if self.type = 0
-        pygame.draw.circle(surface) #color position, radius)
+    def display_enemy(self, surface): # Displays enemy to screen
+        pass
 
     
