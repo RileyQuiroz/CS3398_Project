37730251cb74eeff1bac9e2b171ffabf2d8ee043
Riley Quiroz <fjq11@txstate.edu>
2024-10-16 21:45:12 -0500

    SCRUM-73 Main game loop now uses enemy type A

--- characters/enemies/enemy_type_a.py
+++ characters/enemies/enemy_type_a.py
@@ -1,2 +1,11 @@
 import pygame
 from characters.enemies.enemy_structure import Enemy
+
+class EnemyTypeA(Enemy):
+    def __init__(self, x, y):
+        super().__init__(3, 0, x, y)
+        self.size = 25  
+        self.velocity = 4  
+        self.image = pygame.Surface((self.size, self.size))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
