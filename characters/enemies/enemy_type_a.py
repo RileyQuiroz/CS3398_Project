38124ccff07e005cedac2f0c697abbd3ab735003
Riley Quiroz <fjq11@txstate.edu>
2024-10-16 23:34:49 -0500

    Modified projectile move speed and frequency, as well as enemy move speed

--- characters/enemies/enemy_type_a.py
+++ characters/enemies/enemy_type_a.py
@@ -6,13 +6,13 @@ class EnemyTypeA(Enemy):
     def __init__(self, x, y, left_bound, right_bound):
         super().__init__(3, 0, x, y)
         self.size = 25  
-        self.velocity = 4
+        self.velocity = 3
         self.left_bound = left_bound
         self.right_bound = right_bound  
         self.image = pygame.Surface((self.size, self.size))
         self.image.fill(self.color)
         self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
-        self.fire_delay = 500  # Time between shots (in milliseconds)
+        self.fire_delay = 800  # Time between shots (in milliseconds)
         self.last_shot_time = pygame.time.get_ticks()  # Time since the last shot
         
     def fire_shot(self, proj_group):
