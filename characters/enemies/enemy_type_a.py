e56b5d76644e457245f1b514a71dcfcaea22ad39
Riley Quiroz <fjq11@txstate.edu>
2024-10-16 22:05:50 -0500

    Enemy type A now fires a basic projectile

--- characters/enemies/enemy_type_a.py
+++ characters/enemies/enemy_type_a.py
@@ -1,5 +1,6 @@
 import pygame
 from characters.enemies.enemy_structure import Enemy
+from characters.enemies.enemy_projectile import EnemyProjectile
 
 class EnemyTypeA(Enemy):
     def __init__(self, x, y):
@@ -9,3 +10,13 @@ class EnemyTypeA(Enemy):
         self.image = pygame.Surface((self.size, self.size))
         self.image.fill(self.color)
         self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
+        self.fire_delay = 500  # Time between shots (in milliseconds)
+        self.last_shot_time = pygame.time.get_ticks()  # Time since the last shot
+        
+    def fire_shot(self, proj_group):
+        current_time = pygame.time.get_ticks()
+        # Check if enough time has passed since the last shot
+        if current_time - self.last_shot_time >= self.fire_delay:
+            projectile = EnemyProjectile(self.rect.centerx, self.rect.centery)
+            proj_group.add(projectile)
+            self.last_shot_time = current_time
