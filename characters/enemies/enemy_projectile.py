1bccf487043ff74745d0e6888e91c57d052ec485
Riley Quiroz <fjq11@txstate.edu>
2024-10-16 23:34:49 -0500

    Modified projectile move speed and frequency, as well as enemy move speed

--- characters/enemies/enemy_projectile.py
+++ characters/enemies/enemy_projectile.py
@@ -1,9 +1,9 @@
 import pygame
 class EnemyProjectile(pygame.sprite.Sprite):
-    def __init__(self, x, y, speed=5):
+    def __init__(self, x, y, speed=3):
         super().__init__()
         self.image = pygame.Surface((5, 10))  # Width 5, height 10 for the projectile
-        self.image.fill((255, 255, 255))  # White color for the projectile
+        self.image.fill((255, 0, 0))  # White color for the projectile
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = speed
         
