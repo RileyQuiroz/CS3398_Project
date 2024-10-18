4a65851fd2574097e4c169d8de82a940fe09485d
Riley Quiroz <fjq11@txstate.edu>
2024-10-18 17:30:43 -0500

    SCRUM-73 Enemies now have sound effects for firing projectile and destruction
    used 200 Free SFX library from itch.io

--- characters/enemies/enemy_type_a.py
+++ characters/enemies/enemy_type_a.py
@@ -15,13 +15,14 @@ class EnemyTypeA(Enemy):
         self.fire_delay = 800  # Time between shots (in milliseconds)
         self.last_shot_time = pygame.time.get_ticks()  # Time since the last shot
         
-    def fire_shot(self, proj_group): # Fires a single bullet
+    def fire_shot(self, proj_group, proj_sound): # Fires a single bullet
         current_time = pygame.time.get_ticks()
         # Check if enough time has passed since the last shot
         if current_time - self.last_shot_time >= self.fire_delay and self.living == True:
             projectile = EnemyProjectile(self.rect.centerx, self.rect.centery)
             proj_group.add(projectile)
             self.last_shot_time = current_time
+            proj_sound.play()
     
     def update(self): # Updates position, will move left and right between specific values
         if self.living == True:
