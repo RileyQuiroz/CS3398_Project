13945e5ba1a5f612a0e46d96af8dedceae123617
ukp7 <ukp7@txstate.edu>
2024-10-27 20:22:25 -0500

    projectile edits
    projectiles are now green instead of red, to avoid confusion with enemy projectiles

--- projectiles/projectiles.py
+++ projectiles/projectiles.py
@@ -8,7 +8,7 @@ class Projectile(pygame.sprite.Sprite):
         self.speed = 7
         self.width = 5
         self.height = 10
-        self.color = (255, 0, 0)  # Bullet color
+        self.color = (0, 255, 0)  # Bullet color
         self.image = pygame.Surface((self.width, self.height))
         self.image.fill(self.color)
         self.rect = self.image.get_rect(center=(self.x, self.y))
