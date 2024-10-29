7feab9a4c26137e004ff07b8e6550d21b2fd4e56
Riley Quiroz <fjq11@txstate.edu>
2024-10-29 01:37:40 -0500

    SCRUM-74 Enemies spawn out of bounds and move down onto their pathway

--- characters/enemies/enemy_type_a.py
+++ characters/enemies/enemy_type_a.py
@@ -12,20 +12,23 @@ class EnemyTypeA(Enemy):
         self.image = pygame.Surface((self.size, self.size))
         self.image.fill(self.color)
         self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
-        self.fire_delay = 1  # Time between shots (in milliseconds)
+        self.fire_delay = 0.9  # Time between shots
         self.last_shot_time = current_time  # Time since the last shot
         
     def fire_shot(self, proj_group, proj_sound, paused, curr): # Fires a single bullet
         current_time = curr
         # Check if enough time has passed since the last shot
-        if current_time - self.last_shot_time >= self.fire_delay and self.living == True and paused == False:
+        if (current_time - self.last_shot_time >= self.fire_delay and self.living == True and paused == False):
             projectile = EnemyProjectile(self.rect.centerx, self.rect.centery)
             proj_group.add(projectile)
             self.last_shot_time = current_time
             proj_sound.play()
     
-    def update(self, paused): # Updates position, will move left and right between specific values
-        if self.living == True and paused == False:
+    def update(self, paused): # Updates position, will move left and right between specific values, and moves down upon spawning
+        if (self.pos_y < self.spawn_destination_y and paused == False):
+            self.rect.y += self.velocity 
+            self.pos_y += self.velocity
+        elif (self.living == True and paused == False):
             if (self.rect.centerx >= self.right_bound or self.rect.centerx <= self.left_bound):
                 self.velocity *= -1
             self.rect.x += self.velocity
