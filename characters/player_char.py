c0e5f4473d075f6c57751116119ccfa82748a081
ukp7 <ukp7@txstate.edu>
2024-10-31 11:11:36 -0500

    player now has health
    health attribute is added
    
    health bar added and should be drawn to screen
    
    def for player to take dmg

--- characters/player_char.py
+++ characters/player_char.py
@@ -5,7 +5,7 @@ import pygame
 from projectiles.projectiles import Projectile
 
 class CharacterPawn:
-    def __init__(self, x, y, projectiles_group, screen_width, screen_height):
+    def __init__(self, x, y, projectiles_group, screen_width, screen_height, health = 100):
         # Initialize character position, movement attributes, and screen dimensions
         self.x = x
         self.y = y
@@ -15,6 +15,10 @@ class CharacterPawn:
         self.projectiles_group = projectiles_group  # Group for handling projectiles
         self.screen_width = screen_width
         self.screen_height = screen_height
+        ##stats
+        self.health = health # this adds the health stat
+        self.is_alive = True
+
 
     def handle_input(self):
         # Handle basic movement input
@@ -47,3 +51,23 @@ class CharacterPawn:
     def draw(self, screen):
         # Draw character pawn on the screen
         pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y, self.width, self.height))
+
+        ##draw health bar
+        self.draw_health_bar(screen)
+
+    def draw_health_bar(self, screen):
+        bar_width = self.width
+        bar_height = 5
+        fill = (self.health / 100) * bar_width
+        health_bar = pygame.Rect(self.x, self.y - 10, bar_width, bar_height)
+        health_fill = pygame.Rect(self.x, self.y - 10, fill, bar_height)
+        
+        pygame.draw.rect(screen, (255, 0, 0), health_bar)
+        pygame.draw.rect(screen, (0, 255, 0), health_fill)
+
+    def take_dmg(self, amount):
+        self.health -= amount
+        if self.health <=0:
+            self.health = 0
+            self.is_alive = False
+
