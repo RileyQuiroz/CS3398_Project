3bad6dddc552599da4236703c5751fdc791269be
Riley Quiroz <fjq11@txstate.edu>
2024-11-02 01:13:37 -0500

    SCRUM-77 Enemy now moves in an arc pattern

--- characters/enemies/enemy_type_b.py
+++ characters/enemies/enemy_type_b.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from characters.enemies.enemy_structure import Enemy
 from projectiles.enemy_projectile import EnemyProjectile
 from projectiles.enemy_projectile_angled import EnemyProjectileAngled
@@ -15,6 +16,10 @@ class EnemyTypeB(Enemy):
         self.rect = self.image.get_rect(center=(self.pos_x, self.pos_y))
         self.fire_delay = 1.4  # Time between shots
         self.last_shot_time = current_time  # Time since the last shot
+        self.movement_angle = 0
+        self.movement_radius = right_bound - x
+        self.movement_direction = 0.05
+        self.movement_center = x
         
     def fire_shot(self, proj_group, paused, curr): # Fires a spread of bullets
         current_time = curr
@@ -29,15 +34,22 @@ class EnemyTypeB(Enemy):
             proj_group.add(angled_projectile_b)
             self.last_shot_time = current_time
     
+    
     def update(self, paused): # Updates position, will move left and right between specific values, and moves down upon spawning
-        if (self.pos_y < self.spawn_destination_y and self.heading_home == False and paused == False):
+        # Move into position
+        if(self.pos_x < self.right_bound+20 and self.heading_home == False and paused == False):
+            self.rect.x += self.velocity 
+            self.pos_x += self.velocity
+        elif (self.pos_y < self.spawn_destination_y and self.heading_home == False and paused == False):
             self.rect.y += self.velocity 
             self.pos_y += self.velocity
-        # This elif will be moved to enemy b when it is complete
         elif (self.heading_home == True and self.living == True and paused == False):
             self.rect.y -= 2 
             self.pos_y -= 2
-        #elif (self.living == True and paused == False):
-        #    if (self.rect.centerx >= self.right_bound or self.rect.centerx <= self.left_bound):
-        #        self.velocity *= -1
-        #    self.rect.x += self.velocity
+        elif (self.living == True and paused == False):
+            self.movement_angle += self.movement_direction
+            if (self.movement_angle >= math.pi or self.movement_angle <= 0):  # math.pi radians = 180 degrees
+                self.movement_direction = -1 * self.movement_direction
+            # Update position along the circular arc
+            self.rect.x = self.movement_center + int(self.movement_radius * math.cos(self.movement_angle))
+            self.rect.y = self.pos_y + int(self.movement_radius * math.sin(self.movement_angle))
