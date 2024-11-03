c837ca1d6573072b122f02a2177723d362a3ac62
Joseph Sheraden-Urrutia <jas759@txstate.edu>
2024-11-03 01:48:49 -0600

    SCRUM-79: Adjusted position of obstacles so that one stays on-screen for testing purposes.

--- menu/main_menu.py
+++ menu/main_menu.py
@@ -45,7 +45,7 @@ leaderboard = Leaderboard("time_scoreboard.json")
 BOULDER_PATH = "assets/objects/spr_boulder_0.png"
 obstacle_group = [
     Mover((200, 200), (10, 10), BOULDER_PATH),
-    Rotator((200, 400), BOULDER_PATH),
+    Rotator((300, 300), BOULDER_PATH),
     ZigZag((0, 300), (50, 0), BOULDER_PATH)
 ]
 
