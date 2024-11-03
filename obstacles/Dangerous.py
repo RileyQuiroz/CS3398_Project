61e47f6a7e24c4cdd77d90822e908e3a7bb85862
Joseph Sheraden-Urrutia <jas759@txstate.edu>
2024-11-03 14:18:57 -0600

    SCRUM-79: Fixed player collision to properly decrement health using player class functions and to make the player flash upon colliding with a Dangerous obstacle.

--- obstacles/Dangerous.py
+++ obstacles/Dangerous.py
@@ -1,10 +1,15 @@
 from obstacles.Obstacle import Obstacle
 
+# The Dangerous class represents an obstacle that deals
+# damage to the player upon collision
 class Dangerous(Obstacle):
     def __init__(self, position, sprite_path):
         super().__init__(position, sprite_path)
     
     def handle_player_collision(self, player):
         super().handle_player_collision(player)
+        player.take_dmg(1)
 
-        player.health -= 1
+    def update(self, player, dt):
+        super().update(player, dt)
+        player.got_hit = self.is_colliding
