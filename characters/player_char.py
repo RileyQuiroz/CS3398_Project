e4cd22adb846015778c8b843852493b67151dd19
ukp7 <ukp7@txstate.edu>
2024-11-17 14:22:25 -0600

    SCRUM-98 shield asset updates
    resized the shield visual asset. also added 2 new potential versions. will test them out when more visuals are added to ensure there will be no confusion

--- characters/player_char.py
+++ characters/player_char.py
@@ -130,7 +130,11 @@ class Consumable(pygame.sprite.Sprite):
         # logic for the appearance based on the type of consumbale
         if consumable_type == "shield_pack":
             ##self.image.fill((0,244,244)) ## this can be used for testing
-            self.image = pygame.image.load("assets/objects/Item_Shield.png").convert_alpha()
+            image = pygame.image.load("assets/objects/Item_Shield3.png").convert_alpha()
+            # TO RESIZE THE ASSET CHANGE width and height 
+            shield_width = 35
+            shield_height = 35
+            self.image = pygame.transform.scale(image, (shield_width, shield_height))
         elif consumable_type == "repair_kit":
             self.image.fill((255, 255, 0))
         else:
